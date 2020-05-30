f3b966ceb8a448a1314e1d1208ae787ef2024cd7
Arturo Ruvalcaba <a.ruvham@gmail.com>
2020-05-30 01:28:21 -0400

    Update linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
       "no-bitwise": 0,
       "no-console": 0,
       "no-extend-native": 0,
+      "no-lonely-if": 0,
       "no-plusplus": 0,
       "prefer-destructuring": 0
     }
